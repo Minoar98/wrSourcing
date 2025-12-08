4dd888994fc6135bf37db733d7fd77a746c42785
Minoar98 <minoarhasan@gmail.com>
2025-12-08 14:04:56 +0600

    bolb footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 border-t border-gray-800 py-8 px-4">
       <div className="max-w-7xl mx-auto text-center">
-        <p className="text-gray-400">Copyright 2024, WR Sourcing</p>
-        <p className="text-gray-500 text-sm mt-2">
-          Developed By <span className="text-orange-500">Minoar</span>
+        <p className="text-gray-400 font-bold">Copyright 2024, WR Sourcing</p>
+        <p className="text-gray-500 text-sm mt-2 font-bold">
+          Developed By <span className="text-orange-500 font-bold">Minoar</span>
         </p>
       </div>
     </footer>
